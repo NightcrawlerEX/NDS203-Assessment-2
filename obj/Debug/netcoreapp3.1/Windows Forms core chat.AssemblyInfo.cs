5cd5fab0af2b6410e6be0c7ec3dc6d288c292ec7
NightcrawlerEX <jsimp314159@gmail.com>
2026-07-23 08:16:40 +1000

    more

--- obj/Debug/netcoreapp3.1/Windows Forms core chat.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/Windows Forms core chat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Windows Forms core chat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+117e2ac1d1b64bba0bfd609a628eca7493743ae2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add781acbd7373c1b7893ff61676c93126f2c90a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Windows Forms core chat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Windows Forms core chat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 Student ID: A00125081
 
+To run this project we needed to install .NET Core 3.1 Desktop runtime
+https://dotnet.microsoft.com/en-us/download/dotnet/3.1
 
 To run this
 
